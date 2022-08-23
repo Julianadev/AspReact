03b2f6dfaa6186f892a7ceae366478d5f2504f38
Julianadev <juramos266@gmail.com>
2022-08-23 17:49:24 -0300

    app

---
+++ AspReact/ClientApp/src/components/Home.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+export class Home extends Component {
+  static displayName = Home.name;
+
+  render() {
+    return (
+      <div>
+        <h1>Consultoria Hank</h1>
+        <p>Bem-Vindos(as) a nossa página</p>
+            
+      </div>
+    );
+  }
+}
